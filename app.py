9b7477ae6d5822ada526df92c7da0ba8964e539c
Jaden Baptista <jadenguitarman@gmail.com>
2019-06-27 13:51:45 -0400

    search

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 from flask import Flask, request
 from werkzeug.utils import secure_filename as secure
-import tensorflow, json
+import tensorflow, cv2
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<form method='post' action='/search'><input name='to_search' type='file'><input type='submit'></form>"
+    return "<form method='post' action='/search'><input name='to_search' type='file' enctype='multipart/form-data'><input type='submit'></form>"
 	
 @app.route("/search", methods=["POST"])
 def search():
 	file = request.files.to_dict(flat=True)["to_search"]
 	print(file)
-	if (file.filename == ""):
-		return "a";
-	if ("." not in file.filename):
-		return "b"; 
-	if (file.filename.split('.')[-1].lower() not in ["png", "jpeg", "jpg"]):
-		return "c"
-	
-	filename = secure(file.filename)
+	if (file.filename == "") or ("." not in file.filename) or (file.filename.split('.')[-1].lower() not in ["png", "jpeg", "jpg"]):
+		return "[]"
+	file.filename = secure(file.filename)	
+	img = cv2.imdecode(numpy.fromstring(file.read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
 	return filename
 	
 if __name__ == '__main__':
