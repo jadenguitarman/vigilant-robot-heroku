f4983026fee62515d50c408026bfdcee19a1891e
Jaden Baptista <jadenguitarman@gmail.com>
2019-06-27 12:46:59 -0400

    modules

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.utils import secure_filename as secure
 import tensorflow
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Home page"
-
+	
+@app.route("/search", methods=["POST"])
+def search():
+	if "file" not in request.files or not request.files[0] or request.files[0].filename == "" or "." not in request.files[0].filename or request.files[0].filename.rsplit('.', 1)[1].lower() not in ["png", "jpeg", "jpg"]:
+		return "[]"
+	file = request.files[0]
+	filename = secure(file.filename)
+	return filename
+	
 if __name__ == '__main__':
 	app.run(use_reloader=True)
