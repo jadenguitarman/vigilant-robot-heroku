d31db440b1fb893951cc5698f0f9730e32f4b831
Jaden Baptista <jadenguitarman@gmail.com>
2019-06-27 19:37:15 -0400

    bug

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 from werkzeug.utils import secure_filename as secure
-import tensorflow, cv2
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import cv2
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,6 +25,39 @@ def search():
 		return filename
 	else:
 		return "Hi there. This endpoint is restricted to POST requests, so please check the docs if you're trying to use the Vigilant Robot REST API. Thanks!"
-	
+
+@app.route("/train")
+def train():
+	numpy.random.seed(7)
+	fashion_mnist = keras.datasets.fashion_mnist
+	(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+	class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+	train_images = train_images / 255.0
+	test_images = test_images / 255.0
+	model = keras.Sequential([
+		keras.layers.Flatten(input_shape=(28, 28)),
+		keras.layers.Dense(128, activation=tf.nn.relu),
+		keras.layers.Dense(10, activation=tf.nn.softmax)
+	])
+	model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+	model.fit(train_images, train_labels, epochs=5)
+	test_loss, test_acc = model.evaluate(test_images, test_labels)
+	print('Test accuracy:', test_acc)
+	model_json = model.to_json()
+	with open("model.json", "w") as json_file:
+		json_file.write(model_json)
+	# serialize weights to HDF5
+	model.save_weights("model.h5")
+	print("Saved model to disk")
+	return "Saved"
+
+@app.route("/model.json")
+def json():
+	return app.send_static_file("model.json")
+
+@app.route("/model.h5")
+def h5():
+	return app.send_static_file("model.h5")
+
 if __name__ == '__main__':
 	app.run(use_reloader=True, debug=True)
