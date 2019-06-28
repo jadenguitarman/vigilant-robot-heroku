167ed2f3f2c7e9e1739c806c0d1f98b73f9d2b79
Jaden Baptista <jadenguitarman@gmail.com>
2019-06-28 11:43:40 -0400

    train

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def train():
 	print('Test accuracy:', test_acc)
 	model_json = model.to_json()
 	model_weights = model.get_weights()
-	to_return = model_json + "\n\n\n\n\n\n\n\n\n\n" + pickle.dumps(model_weights).decode('latin1')
+	to_return = model_json + "\n\n\n\n"+str(model_weights.shape)+"\n"+str(model_weights.dtype)+"\n\n\n\n\n\n" + model_weights.tostring()
 	return to_return
 
 if __name__ == '__main__':
