780e923aad7d6153f53784cd2f2d797318edc72d
Jaden Baptista <jadenguitarman@gmail.com>
2019-06-28 14:10:15 -0400

    prediction

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def search():
 		class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'] 
 		prediction = model.predict(img)[0]
 		prediction.dtype = float
-		prediction = json.dumps(dict(zip([class_names[x] for x in prediction], prediction)))
+		prediction = json.dumps(dict(zip(class_names, prediction)))
 		
 		return prediction
 	else:
