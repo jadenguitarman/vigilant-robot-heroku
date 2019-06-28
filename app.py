5e627803bd30fbd3b12b3f00d06cbcf4ae12e2e7
Jaden Baptista <jadenguitarman@gmail.com>
2019-06-27 20:43:31 -0400

    tmp

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def train():
 	np.random.seed(7)
 	fashion_mnist = keras.datasets.fashion_mnist
 	(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-	class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+	class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'] 
 	train_images = train_images / 255.0
 	test_images = test_images / 255.0
 	model = keras.Sequential([
